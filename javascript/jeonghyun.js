12e5c0dc4aec5804a786cb093244e2c91d3bf983
jeonghyun <woose1390@gamil.com>
2022-11-16 23:57:36 +0900

    Jeonghyun modify 221116

--- javascript/jeonghyun.js
+++ javascript/jeonghyun.js
@@ -1,24 +1,19 @@
 $(document).ready(function(){
-
-    // $(".faq > ul").click(function(){
-    //     $(this).children.toggleClass("on");
-    // });
-    // console.log($(".faq > ul").children(".a").hasClass("on"));
-    
     $(".faq .question .q").click(function(){
-        // console.log($(this));
          if($(this).siblings(".a").hasClass("on")){
         $(this).siblings(".a").stop().slideUp();
         $(this).siblings(".a").removeClass("on");
-        console.log($(this.sibling));
+        $(this).css("border","");
        }
        else{
         $(this).siblings(".a").stop().slideDown();
         $(this).siblings(".a").addClass("on");
+        $(this).css("border","1px solid black").css("border-radius","3px");
+        $(this).parents().siblings().children(".a").removeClass("on");
+        $(this).parents().siblings().children(".a").slideUp();
+        $(this).parents().siblings().children(".q").css("border","").css("border-radius","");
         console.log($(this));
        }
-      
-        // console.log( $(".faq > .q").children("li").hasClass("on"))
     });
     })
     
