6103bb32b1b64447a4a624a73e5c40d9033b7cbc
jeonghyun <woose1390@gamil.com>
2022-11-17 23:30:59 +0900

    Jeonghyun modify 221117

--- javascript/jeonghyun.js
+++ javascript/jeonghyun.js
@@ -1,21 +1,18 @@
 $(document).ready(function(){
     $(".faq .question .q").click(function(){
-         if($(this).siblings(".a").hasClass("on")){
-        $(this).siblings(".a").stop().slideUp();
-        $(this).siblings(".a").removeClass("on");
-        $(this).css("border","");
-       }
-       else{
-        $(this).siblings(".a").stop().slideDown();
-        $(this).siblings(".a").addClass("on");
-        $(this).css("border","1px solid black").css("border-radius","3px");
-        $(this).parents().siblings().children(".a").removeClass("on");
-        $(this).parents().siblings().children(".a").slideUp();
-        $(this).parents().siblings().children(".q").css("border","").css("border-radius","");
-        console.log($(this));
-       }
+            if($(this).siblings(".a").hasClass("on")){
+            $(this).siblings(".a").stop().slideUp().removeClass("on");
+            $(this).css("border","").css("border-radius","").css("font-weight","");
+        }
+
+        else{
+            $(this).siblings(".a").stop().slideDown().addClass("on");
+            $(this).css("border","1px solid black").css("border-radius","3px").css("font-weight","bold");
+            $(this).parents().siblings().children(".a").removeClass("on").slideUp();
+            $(this).parents().siblings().children(".q").css("border","").css("border-radius","").css("font-weight","");
+        }
     });
-    })
+})
     
     
     
