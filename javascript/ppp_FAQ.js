6ea88c817424009b2694f290fdcbbef2a3698361
kin698 <kin698@gmail.com>
2022-11-18 13:46:42 +0900

    Chanhyeong 221118

--- javascript/ppp_FAQ.js
+++ javascript/ppp_FAQ.js
@@ -1,4 +1,4 @@
-$(function(){
+$(document).ready(function(){
     $(".menu>li>a").click(function(){
     $(this).next("p").slideToggle(200);
     $(this).parent("li").siblings("li").children("p").slideUp(200);
